6a345d35a71e0dbafc30045f0143c016199a8027
Nhan pham <nhan.qc2@gmail.com>
2018-11-12 23:07:21 +0700

    mem 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.printf("second line");
+        System.out.printf("gbtbrthbrthn line");
+        System.out.printf("abcd");
     }
 }
